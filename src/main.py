e4e4053b94491660f020e7af2b200d84b35e8798
Charlton Lim <cadmusthefounder@gmail.com>
2017-09-15 02:30:29 +0800

    Formatting changes

--- src/main.py
+++ src/main.py
@@ -14,15 +14,15 @@ def main():
     padded_plaintext = pkcs7_pad(plaintext, BLOCK_SIZE)
     ciphertext = oracle.aes_padding(padded_plaintext, IV)
 
-    print "\nPlaintext Entered: {}. Number of bytes: {}.".format(plaintext, len(plaintext))
-    print "Padded Plaintext: {}. Number of bytes: {}.".format(padded_plaintext, len(padded_plaintext))
-    print "Ciphertext: {}. Number of bytes: {}.\n".format(ciphertext, len(ciphertext))
+    print "\nPlaintext Entered: {} Number of bytes: {}".format(plaintext, len(plaintext))
+    print "Padded Plaintext: {} Number of bytes: {}".format(padded_plaintext, len(padded_plaintext))
+    print "Ciphertext: {} Number of bytes: {}\n".format(repr(ciphertext), len(ciphertext))
 
     print "Executing attack..."
     decoded_ciphertext = execute_padding_oracle_attack(oracle, ciphertext, IV)
-    print "Attack has commenced.\n"
+    print "Attack has ended.\n"
 
-    print "Decoded Ciphertext: {}. Number of bytes: {}".format(decoded_ciphertext, len(decoded_ciphertext))
+    print "Decoded Ciphertext: {} Number of bytes: {}".format(decoded_ciphertext, len(decoded_ciphertext))
     print "\n---------- End of Padding Oracle Attack ----------\n"
 
 if __name__ == '__main__':
